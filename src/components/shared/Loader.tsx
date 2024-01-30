07c11614471b0092cc157b4d15571a062c195597
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-01-30 21:25:21 +0530

    Update Loader.tsx

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => (
     <div className="flex-center w-full">
       <img
-        src="assets/icons/loader.svg"
+        src="/assets/icons/loader.svg"
         alt="loader"
         width={24}
         height={24}
