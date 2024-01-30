0dd61371f4693cac461e28f8b697d5e0162712bd
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-01-30 11:27:11 +0530

    Update baseQuery.ts

--- src/store/api/baseQuery.ts
+++ src/store/api/baseQuery.ts
@@ -2,7 +2,7 @@
 
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080/" }); 
+export const baseQuery = fetchBaseQuery({ baseUrl: "https://clubhubb-user.onrender.com/" }); 
 // export const baseQuery = fetchBaseQuery({ baseUrl: "https://clubhub-backend-57s4.onrender.com/" }); 
 
 
