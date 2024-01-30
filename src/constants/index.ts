81062fb807431635e08aa4feb3165162498e88ff
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-01-30 21:21:02 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -24,22 +24,22 @@ export const sidebarLinks = [
 
 export const bottombarLinks = [
     {
-        imgURL: "assets/icons/home.svg",
+        imgURL: "/assets/icons/home.svg",
         route: "/",
         label: "Home",
     },
     {
-        imgURL: "assets/icons/wallpaper.svg",
+        imgURL: "/assets/icons/wallpaper.svg",
         route: "/explore",
         label: "Explore",
     },
     {
-        imgURL: "assets/icons/bookmark.svg",
+        imgURL: "/assets/icons/bookmark.svg",
         route: "/clubs",
         label: "Clubs",
     },
     {
-        imgURL: "assets/icons/people.svg",
+        imgURL: "/assets/icons/people.svg",
         route: "/profile",
         label: "Profile",
     },
