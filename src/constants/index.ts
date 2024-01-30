e67bd938d5412ba205e22961dc3426f222d1b5fd
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-01-30 21:22:39 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,22 +1,22 @@
 export const sidebarLinks = [
     {
-        imgURL: "assets/icons/home.svg",
+        imgURL: "/assets/icons/home.svg",
         route: "/",
         label: "Home",
     },
     {
-        imgURL: "assets/icons/wallpaper.svg",
+        imgURL: "/assets/icons/wallpaper.svg",
         route: "/post",
         label: "Explore",
     },
 
     {
-        imgURL: "assets/icons/bookmark.svg",
+        imgURL: "/assets/icons/bookmark.svg",
         route: "/clubs",
         label: "Clubs",
     },
     {
-        imgURL: "assets/icons/people.svg",
+        imgURL: "/assets/icons/people.svg",
         route: "/profile",
         label: "Profile",
     },
