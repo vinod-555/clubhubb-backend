0cde154384c3e9d9e15a30fc57f79f9229dba2d7
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-02-02 00:35:16 +0530

    Update authApi.ts

--- src/store/api/authApi.ts
+++ src/store/api/authApi.ts
@@ -5,53 +5,78 @@ import { baseQuery } from './baseQuery';
 
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
-  reducerPath: 'authApi',
-  baseQuery ,
-  endpoints: (builder) => ({
-        signinUser:builder.mutation({
-            query:(body:{emailOrUsername:string; password:string})=>{
+    reducerPath: "authApi",
+    baseQuery,
+    endpoints: (builder) => ({
+        signinUser: builder.mutation({
+            query: (body: { emailOrUsername: string; password: string }) => {
                 return {
-                    url:"/api/v1/user-auth/signin",
-                    method:"post",
+                    url: "/api/v1/user-auth/signin",
+                    method: "post",
                     body,
-                }
-            }
+                };
+            },
         }),
-        signupUser:builder.mutation({
-            query:( body:{name:string; username:string;email:string;collegeName:string;phoneNumber:string;password:string})=>{
-
+        signupUser: builder.mutation({
+            query: (body: {
+                name: string;
+                username: string;
+                email: string;
+                collegeName: string;
+                phoneNumber: string;
+                password: string;
+            }) => {
                 return {
-                    url:"/api/v1/user-auth/signup",
-                    method:"post",
-                    body
-                    
-                }
-            }
+                    url: "/api/v1/user-auth/signup",
+                    method: "post",
+                    body,
+                };
+            },
         }),
-        forgotpassword:builder.mutation({
-            query:(body:{clubEmail:string})=>{
-                return{
-                    url:'api/v1/admin-auth/forgot-password',
-                    method:"post",
-                    body
-
-                }
-            }
-
+        forgotpassword: builder.mutation({
+            query: (body: { clubEmail: string }) => {
+                return {
+                    url: "api/v1/user-auth/forgot-password",
+                    method: "post",
+                    body,
+                };
+            },
         }),
         verifyuser: builder.mutation({
-            query: (body:{token:string}) => ({
-              url: 'api/v1/user-auth/protected',
-              method: 'get',
-              headers: {
-                Authorization: `${body.token}`,
-              },
+            query: (body: { token: string }) => ({
+                url: "api/v1/user-auth/protected",
+                method: "get",
+                headers: {
+                    Authorization: `${body.token}`,
+                },
             }),
-          }),  
-  }),
-})
+        }),
+
+        verifyOTP: builder.mutation({
+            query: (body: { email: string; otp: string }) => ({
+                url: "/api/v1/user-auth/verify-otp",
+                method: "post",
+                body,
+            }),
+        }),
+
+        resetPassword: builder.mutation({
+            query: ({ email, password }) => ({
+                url: "api/v1/user-auth/reset-password",
+                method: "post",
+                body: { email, password },
+            }),
+        }),
+    }),
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const {  useSigninUserMutation, useSignupUserMutation, useForgotpasswordMutation, useVerifyuserMutation
- } = authApi
+export const {
+    useSigninUserMutation,
+    useSignupUserMutation,
+    useForgotpasswordMutation,
+    useVerifyuserMutation,
+    useVerifyOTPMutation,
+    useResetPasswordMutation,
+} = authApi;
