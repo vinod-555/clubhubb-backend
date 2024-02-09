8c6837b4c2317d253c649fa849149ecd2489f1cb
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-02-09 11:03:10 +0530

    Update RegisterEvent.tsx

--- src/components/shared/RegisterEvent.tsx
+++ src/components/shared/RegisterEvent.tsx
@@ -378,7 +378,6 @@ export default function RegisterEvent(props: RegisterEventProps) {
                             
                                     <div className="p-4 m-2" style={{backgroundColor: "#0DB14B" }}>
                                     <p>Registered successfully</p>
-                                    <p>Check your email for conformation</p>
                                     <p>Thank You!!!</p>
                                     {/* Add tick icon here */}
                                   </div>
