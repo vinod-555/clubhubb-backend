240bedd754aa588128795df5ced98416c3ac99bb
yvinod29 <129191462+yvinod29@users.noreply.github.com>
2024-02-21 20:03:12 +0530

    Update Explore.tsx

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,13 +1,25 @@
 
 const Explore = () => {
   return (
-    <>
- 
-        <center className="m-2">
+     <div className="items-center ">
+      <div className="font-bold text-1xl md:text-4xl m-5" >
+        <h5>Contact Us</h5>
+        <div  >
+          <div  >
+            <label>Email:</label>
+            <p>clubhubboffical@gmail.com</p>
+          </div>
+          <div  >
+            <label>Number:</label>
+            <p>6302853438</p>
+          </div>
+        </div>
+      </div>
+      <center className="m-2">
            <img src="/assets/images/community.jpg" alt="community"/>
 
          </center>
-    </>
+    </div>
   );
 };
 
